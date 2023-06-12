acc2fd419d96b8950e4f7b212dc94a52b3694d52
fishcakefish <fishcakeow@gmail.com>
2023-06-12 16:54:10 -0700

    dffaafsfa

--- backend/db/seeders/20230612061815-demo-user.js
+++ backend/db/seeders/20230612061815-demo-user.js
@@ -23,16 +23,22 @@ module.exports = {
     {
       email: 'random@gmail.com',
       username: 'NPC',
+      firstName: 'rando',
+      lastName: 'one',
       hashedPassword: bcrypt.hashSync('password')
     },
     {
       email: 'random2@gmail.com',
       username: 'NPC2',
+      firstName: 'rando',
+      lastName: 'two',
       hashedPassword: bcrypt.hashSync('password2')
     },
     {
       email: 'random3@gmail.com',
       username: 'NPC3',
+      firstName: 'rando',
+      lastName: 'three',
       hashedPassword: bcrypt.hashSync('password3')
     }
    ], {})
