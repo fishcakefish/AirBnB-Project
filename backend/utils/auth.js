acd4c00819461e915a39285c0b5da8ff1de3ff97
fishcakefish <fishcakeow@gmail.com>
2023-06-12 16:45:42 -0700

    testttttt

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -9,9 +9,7 @@ const setTokenCookie = (res, user) => {
     const safeUser = {
         id: user.id,
         email: user.email,
-        username: user.username,
-        firstName: user.firstName,
-        lastName: user.lastName
+        username: user.username
     }
     const token = jwt.sign(
         {data: safeUser},
